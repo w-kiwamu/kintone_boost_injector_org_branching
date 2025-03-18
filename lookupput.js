e5e3eb9156471f0f7772d255805e4d791097c461
w-kiwamu <watanabe.kiwamu@gmail.com>
2025-03-18 14:55:17 +0900

    Update lookupput.js

--- lookupput.js
+++ lookupput.js
@@ -1,12 +1,9 @@
 (() => {
-    'use strict';
-    //kintone.events.on('app.record.create.show', (event) => {
-    kb.event.on('kb.create.load', (event) => {
-    alert('あいうえお');
-    console.log('あいうえお');
-    const record = event.record;
-    record['事業所番号'].value = '23004';
-    record['事業所番号'].lookup = true;
-    return event;
-  });
+"use strict";
+const handler = (event) => {
+console.log(event);
+};
+kb.event.on('kb.view.load', handler);
+ 
+kb.event.off('kb.view.load', handler);
 })();

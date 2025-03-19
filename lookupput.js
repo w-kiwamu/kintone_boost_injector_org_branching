b3827d2f049b2c7d64feda84e0a63c962029e063
w-kiwamu <watanabe.kiwamu@gmail.com>
2025-03-19 18:17:55 +0900

    Update lookupput.js

--- lookupput.js
+++ lookupput.js
@@ -2,6 +2,10 @@
 "use strict";
 kb.event.on('kb.create.load', (event) => {
  console.log(event);
+ //record['ルックアップ_0'].value = '0001'; // ルックアップフィールドの値
+ //record['ルックアップ_0'].lookup = true; // またはrecord['ルックアップ_0'].lookup = 'UPDATE';
+ record['事業所番号'].value = '23004'; // ルックアップフィールドの値
+ record['事業所番号'].lookup = true; // またはrecord['事業所番号'].lookup = 'UPDATE';
 return event;
 });
 })();

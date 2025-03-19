ca25dd50982ea22e035b697f74f5cf29a5044009
w-kiwamu <watanabe.kiwamu@gmail.com>
2025-03-19 18:35:06 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -3,7 +3,11 @@
   kintone.events.on('app.record.create.show', (event) => {
     const record = event.record;
     alert('あいうえお');
-    console.log('あいうえお');
+    console.log(event);
+    
+    record['branch_ID'].value = '23004'; // ルックアップフィールドの値
+    record['branch_ID'].lookup = true; // またはrecord['branch_ID'].lookup = 'UPDATE';
+    
     //record['Hello'].value = 'こんにちは、kintone!';
     return event;
   });

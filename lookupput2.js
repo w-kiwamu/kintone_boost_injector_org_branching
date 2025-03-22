f5ad742ced3fd65bde2000b3e5777fd10d3e6581
w-kiwamu <watanabe.kiwamu@gmail.com>
2025-03-22 16:57:35 +0900

    Update lookupput2.js

--- lookupput2.js
+++ lookupput2.js
@@ -6,7 +6,11 @@ kb.event.on('kb.create.load', (event) => {
 
 // 別の変数を使用してイベントを操作
 const myEvent = event;
-//myEvent.record['備考'].value = 'この文字列で上書き';
+
+//URLの最後の部分を取得
+const pathname = window.location.pathname;
+ 
+//myEvent.record['備考'].value = 'この文字列で書き';
 
 myEvent.record['抽出ID'].value = pathname.replaceAll("/", "").replace("asc2test", ""); // ルックアップフィールドの値
 myEvent.record['抽出ID'].lookup = true; // アップデート

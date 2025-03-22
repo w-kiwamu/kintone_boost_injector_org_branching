67b2bff133b07fec71f37fce6fbae2ab67089a9c
w-kiwamu <watanabe.kiwamu@gmail.com>
2025-03-22 13:05:00 +0900

    Update lookupput.js

--- lookupput.js
+++ lookupput.js
@@ -4,23 +4,23 @@
 "use strict";
 kb.event.on('kb.create.load', (event) => {
 //kb.event.on('kb.edit.load', (event) => {
- console.log(event);
+ console.log('取得したイベントの情報:', event);
  //alert('aaaaa');
  //record['ルックアップ_0'].value = '0001'; // ルックアップフィールドの値
  //record['ルックアップ_0'].lookup = true; // またはrecord['ルックアップ_0'].lookup = 'UPDATE';
  //record['branch_ID'].value = '23004'; // ルックアップフィールドの値
 
 // エラーメッセージを詳しく確認
-try {
-  event.record['自由記載'].value = 'この文字列で上書き';
-} catch (e) {
-  console.error('エラーの詳細:', e);
-}
+//try {
+//  event.record['自由記載'].value = 'この文字列で上書き';
+//} catch (e) {
+//  console.error('エラーの詳細:', e);
+//}
 
 // イベントオブジェクトの構造を確認
-console.log('イベントオブジェクト:', JSON.stringify(event));
-console.log('recordプロパティ:', event.record);
-console.log('自由記載フィールド:', event.record['自由記載']);
+//console.log('イベントオブジェクト:', JSON.stringify(event));
+//console.log('recordプロパティ:', event.record);
+//console.log('自由記載フィールド:', event.record['自由記載']);
 
 // 別の変数を使用して試してみる
 const myEvent = event;

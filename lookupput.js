3d4a0d727ba6e93007a6eff223cbcbc2966105e8
w-kiwamu <watanabe.kiwamu@gmail.com>
2025-03-22 13:11:26 +0900

    Update lookupput.js

--- lookupput.js
+++ lookupput.js
@@ -10,27 +10,14 @@ kb.event.on('kb.create.load', (event) => {
  //record['ルックアップ_0'].lookup = true; // またはrecord['ルックアップ_0'].lookup = 'UPDATE';
  //record['branch_ID'].value = '23004'; // ルックアップフィールドの値
 
-// エラーメッセージを詳しく確認
-//try {
-//  event.record['自由記載'].value = 'この文字列で上書き';
-//} catch (e) {
-//  console.error('エラーの詳細:', e);
-//}
 
-// イベントオブジェクトの構造を確認
-//console.log('イベントオブジェクト:', JSON.stringify(event));
-//console.log('recordプロパティ:', event.record);
-//console.log('自由記載フィールド:', event.record['自由記載']);
 
 // 別の変数を使用して試してみる
 const myEvent = event;
 myEvent.record['自由記載'].value = 'この文字列で上書き';
 
- 
-
-
- 
- //event.record['自由記載'].value = 'この文字列で上書き';
+myEvent.record['branch_ID'].value = '23004'; // ルックアップフィールドの値
+myEvent.record['branch_ID'].lookup = true; // またはrecord['branch_ID'].lookup = 'UPDATE';
  //record['branch_ID'].lookup = true; // またはrecord['branch_ID'].lookup = 'UPDATE';
 return event;
 });

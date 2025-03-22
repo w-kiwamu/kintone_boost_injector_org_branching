fbc6d9b0ef4d45258b0b8309737ee6fe3b431e91
w-kiwamu <watanabe.kiwamu@gmail.com>
2025-03-22 11:35:08 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -7,6 +7,8 @@
     
     record['branch_ID'].value = '23004'; // ルックアップフィールドの値
     record['branch_ID'].lookup = true; // またはrecord['branch_ID'].lookup = 'UPDATE';
+
+    record['自由記載'].value = 'この文字列で上書き';
     
     //record['Hello'].value = 'こんにちは、kintone!';
     return event;

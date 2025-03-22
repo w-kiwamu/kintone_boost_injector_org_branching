f7e55e869d0c3c33f3138e68b3b089ff1e4ac932
w-kiwamu <watanabe.kiwamu@gmail.com>
2025-03-22 16:19:19 +0900

    Create lookupput2.js

---
+++ lookupput2.js
@@ -0,0 +1,23 @@
+//  https://w-kiwamu.github.io/kintone_boost_injector_org_branching/lookupput2.js
+
+(() => {
+"use strict";
+kb.event.on('kb.create.load', (event) => {
+
+// パス部分（例：/page）
+const pathname = window.location.pathname;
+console.log(pathname.replaceAll("/", ""));
+ 
+console.log('取得したイベントの情報:', event);
+//alert('aaaaa');
+
+// 別の変数を使用してイベントを操作
+const myEvent = event;
+myEvent.record['自由記載'].value = 'この文字列で上書き';
+
+myEvent.record['branch_ID'].value = '23004'; // ルックアップフィールドの値
+myEvent.record['branch_ID'].lookup = true; // アップデート
+ 
+return event;
+});
+})();

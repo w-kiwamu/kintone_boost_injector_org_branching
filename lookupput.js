322d6b5b6cda2084da1a4b90bdbcd577261bb843
w-kiwamu <watanabe.kiwamu@gmail.com>
2025-03-22 12:39:39 +0900

    Update lookupput.js

--- lookupput.js
+++ lookupput.js
@@ -9,7 +9,7 @@ kb.event.on('kb.create.load', (event) => {
  //record['ルックアップ_0'].value = '0001'; // ルックアップフィールドの値
  //record['ルックアップ_0'].lookup = true; // またはrecord['ルックアップ_0'].lookup = 'UPDATE';
  //record['branch_ID'].value = '23004'; // ルックアップフィールドの値
- record['自由記載'].value = 'この文字列で上書き';
+ event.record['自由記載'].value = 'この文字列で上書き';
  //record['branch_ID'].lookup = true; // またはrecord['branch_ID'].lookup = 'UPDATE';
 return event;
 });

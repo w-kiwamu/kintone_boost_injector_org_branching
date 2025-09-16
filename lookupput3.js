7be5bd973ba9f11bf04357a7e107b6b511d19055
w-kiwamu <watanabe.kiwamu@gmail.com>
2025-09-16 12:30:09 +0900

    Update lookupput3.js

--- lookupput3.js
+++ lookupput3.js
@@ -3,7 +3,6 @@
 (() => {
 "use strict";
 kb.event.on('kb.create.load', (event) => {
-console.log(kb.operator.domain); // サブドメインをコンソールに出力
 console.log(Object.keys(kb));
 console.log(kb); // kintone_boosterオブジェクトを表示
 
@@ -15,8 +14,12 @@ console.log(myEvent); // マイイベントを表示
 //URLの最後の部分を取得
 const pathname = window.location.pathname;
 
+//kintoneのサブドメインを取得
+const subdomain = kb.operator.domain;
+console.log(subdomain); // サブドメインをコンソールに出力
+
 //ルックアップフィールド（アプリコードが↓では　抽出ID）にBoost! Injector用URLの最後にあるフォルダ名から、"asc2test"を削除した文字を設定。そうなるようにフォルダ名と抽出IDが一致するよう事前にkintoneアプリを設計する必要がある。
-myEvent.record['抽出ID'].value = pathname.replaceAll("/", "").replace("asc2test", ""); // ルックアップフィールドの値を決める
+myEvent.record['抽出ID'].value = pathname.replaceAll("/", "").replace(subdomain + "-", "").split('-')[0]; // ルックアップフィールドの値を決める
 myEvent.record['抽出ID'].lookup = true; // ルックアップフィールドをInjectorの画面上でアップデート
 myEvent.record['抽出ID'].disabled = true; // ルックアップフィールドをInjectorの画面上で編集不可にする
 

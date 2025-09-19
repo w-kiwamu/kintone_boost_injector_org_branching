2305d2864844423370bfa4a02fe251f419b1e8cb
w-kiwamu <watanabe.kiwamu@gmail.com>
2025-09-19 16:00:03 +0900

    Update lookupput4.js

--- lookupput4.js
+++ lookupput4.js
@@ -26,7 +26,9 @@ myEvent.record['抽出ID'].disabled = true; // ルックアップフィールド
 
 
 //ルックアップフィールド（アプリコードが↓では　最終更新アカウント情報）にBoost! Injector用URLの最後にあるフォルダ名から、subdomain と-を削除した文字を-で分割して左側を抽出IDとして設定。そうなるようにフォルダ名と抽出IDが一致するよう事前にkintoneアプリを設計する必要がある。
+console.log(pathname.replaceAll("/", "")); // マイイベントを表示
 console.log(pathname.replaceAll("/", "").replace(subdomain + "-", "")); // マイイベントを表示
+console.log(pathname.replaceAll("/", "").replace(subdomain + "-", "").split('-')[1]); // ルックアップフィールドの値を決める
 myEvent.record['最終更新アカウント情報'].value = pathname.replaceAll("/", "").replace(subdomain + "-", ""); // ルックアップフィールドの値を決める
 myEvent.record['最終更新アカウント情報'].lookup = true; // ルックアップフィールドをInjectorの画面上でアップデート
 myEvent.record['最終更新アカウント情報'].disabled = true; // ルックアップフィールドをInjectorの画面上で編集不可にする

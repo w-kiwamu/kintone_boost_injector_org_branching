9bd8832b59cca0eddd4382f7db6a3e87cf4a5dd8
w-kiwamu <watanabe.kiwamu@gmail.com>
2025-09-23 12:20:35 +0900

    Update lookupput4.js

--- lookupput4.js
+++ lookupput4.js
@@ -32,6 +32,7 @@ console.log(pathname.replaceAll("/", "")); // マイイベントを表示
 console.log(pathname.replaceAll("/", "").replace(subdomain + "-", "")); // マイイベントを表示
 console.log(pathname.replaceAll("/", "").replace(subdomain + "-", "").split('-')[1]); // ルックアップフィールドの値を決める
 myEvent.record['最終更新アカウント情報'].value = pathname.replaceAll("/", "").replace(subdomain + "-", "").split('-')[1]; // ルックアップフィールドの値を決める
+myEvent.record['最終更新アカウント情報'].lookup = true; // ルックアップフィールドをInjectorの画面上でアップデート
 myEvent.record['最終更新アカウント情報'].disabled = true; // ルックアップフィールドをInjectorの画面上で編集不可にする
 
 

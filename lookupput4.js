a8fbde071c7e6db52cde391a9faae3628cc84524
w-kiwamu <watanabe.kiwamu@gmail.com>
2026-04-20 11:57:30 +0900

    Update lookupput4.js
    kb -> bst
    クラスの更新があったため

--- lookupput4.js
+++ lookupput4.js
@@ -3,9 +3,9 @@
 //新規の場合
 (() => {
 "use strict";
-kb.event.on('kb.create.load', (event) => {
-console.log(Object.keys(kb));
-console.log(kb); // kintone_boosterオブジェクトを表示
+bst.event.on('bst.create.load', (event) => {
+console.log(Object.keys(bst));
+console.log(bst); // kintone_boosterオブジェクトを表示
 
 // 別の変数を使用してイベントを操作
 const myEvent = event;
@@ -16,7 +16,7 @@ console.log(myEvent); // マイイベントを表示
 const pathname = window.location.pathname;
 
 //kintoneのサブドメインを取得
-const subdomain = kb.operator.domain;
+const subdomain = bst.operator.domain;
 console.log(subdomain); // サブドメインをコンソールに出力
 
 //ルックアップフィールド（アプリコードが↓では　抽出ID）にBoost! Injector用URLの最後にあるフォルダ名から、subdomain と-を削除した文字を-で分割して左側を抽出IDとして設定。そうなるようにフォルダ名と抽出IDが一致するよう事前にkintoneアプリを設計する必要がある。
@@ -36,12 +36,12 @@ myEvent.record['最終更新アカウント情報'].value = pathname.replaceAll(
 //myEvent.record['最終更新アカウント情報'].disabled = true; // ルックアップフィールドをInjectorの画面上で編集不可にする
 
 
-//kb.injector.app.fields['事業所'].hideExpression = true; // フィールドをInjectorの画面上で非表示にする
-//kb.injector.setFieldShown('事業所', false); // フィールドを非表示にする
+//bst.injector.app.fields['事業所'].hideExpression = true; // フィールドをInjectorの画面上で非表示にする
+//bst.injector.setFieldShown('事業所', false); // フィールドを非表示にする
 
   
 myEvent.record['主キー'].disabled = true; // ルックアップフィールドをInjectorの画面上で編集不可にする
-//kb.injector.app.fields['主キー'].hideExpression = true; // フィールドをInjectorの画面上で非表示にする
+//bst.injector.app.fields['主キー'].hideExpression = true; // フィールドをInjectorの画面上で非表示にする
   
 return event;
 });
@@ -53,9 +53,9 @@ return event;
 //編集の場合
 (() => {
 "use strict";
-kb.event.on('kb.edit.load', (event) => {
-console.log(Object.keys(kb));
-console.log(kb); // kintone_boosterオブジェクトを表示
+bst.event.on('bst.edit.load', (event) => {
+console.log(Object.keys(bst));
+console.log(bst); // kintone_boosterオブジェクトを表示
 
 // 別の変数を使用してイベントを操作
 const myEvent = event;
@@ -66,7 +66,7 @@ console.log(myEvent); // マイイベントを表示
 const pathname = window.location.pathname;
 
 //kintoneのサブドメインを取得
-const subdomain = kb.operator.domain;
+const subdomain = bst.operator.domain;
 console.log(subdomain); // サブドメインをコンソールに出力
 
 myEvent.record['抽出ID'].disabled = true; // ルックアップフィールドをInjectorの画面上で編集不可にする
@@ -81,14 +81,13 @@ myEvent.record['最終更新アカウント情報'].value = pathname.replaceAll(
 //myEvent.record['最終更新アカウント情報'].lookup = true; // ルックアップフィールドをInjectorの画面上でアップデート
 //myEvent.record['最終更新アカウント情報'].disabled = true; // ルックアップフィールドをInjectorの画面上で編集不可にする
 
-//kb.injector.app.fields['事業所'].hideExpression = true; // フィールドをInjectorの画面上で非表示にする
-//kb.injector.setFieldShown('事業所', false); // フィールドを非表示にする
+//bst.injector.app.fields['事業所'].hideExpression = true; // フィールドをInjectorの画面上で非表示にする
+//bst.injector.setFieldShown('事業所', false); // フィールドを非表示にする
 
   
 myEvent.record['主キー'].disabled = true; // ルックアップフィールドをInjectorの画面上で編集不可にする
-//kb.injector.app.fields['主キー'].hideExpression = true; // フィールドをInjectorの画面上で非表示にする
+//bst.injector.app.fields['主キー'].hideExpression = true; // フィールドをInjectorの画面上で非表示にする
   
 return event;
 });
 })();
-
